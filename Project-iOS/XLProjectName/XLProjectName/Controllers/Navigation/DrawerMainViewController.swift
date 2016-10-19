aa1dc08fa66bcff46eaea1be6e5c27259d00730e
remer <revetria.miguel@gmail.com>
2016-10-19 11:18:24 -0300

    WIP

---
+++ Project-iOS/XLProjectName/XLProjectName/Controllers/Navigation/DrawerMainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DrawerMainViewController.swift
+//  XLProjectName
+//
+//  Created by XLAuthorName ( XLAuthorWebsite )
+//  Copyright (c) 2016 XLOrganizationName. All rights reserved.
+//
+
+import UIKit
+
+class DrawerMainViewController: XLProjectNameNavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
